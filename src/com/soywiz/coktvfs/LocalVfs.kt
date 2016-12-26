b95da86bb7a296d01007595a26e839d695137a09
Carlos Ballesteros Velasco <soywiz@gmail.com>
2016-12-26 20:41:19 +0100

    Proper java-nio asynchronous I/O for open streams

--- src/com/soywiz/coktvfs/LocalVfs.kt
+++ src/com/soywiz/coktvfs/LocalVfs.kt
@@ -1,65 +1,97 @@
 package com.soywiz.coktvfs
 
+import com.soywiz.coktvfs.async.asyncFun
 import com.soywiz.coktvfs.async.asyncGenerate
 import com.soywiz.coktvfs.async.executeInWorker
 import com.soywiz.coktvfs.stream.AsyncStream
 import java.io.File
 import java.io.FileOutputStream
-import java.io.RandomAccessFile
+import java.nio.ByteBuffer
+import java.nio.channels.AsynchronousFileChannel
+import java.nio.channels.CompletionHandler
 import java.nio.file.Files
 import java.nio.file.Paths
+import java.nio.file.StandardOpenOption
+import kotlin.coroutines.suspendCoroutine
 
 fun LocalVfs(base: File): VfsFile {
-    val baseAbsolutePath = base.absolutePath
+	val baseAbsolutePath = base.absolutePath
 
-    class Impl : Vfs() {
-        fun resolve(path: String) = "$baseAbsolutePath/$path"
+	class Impl : Vfs() {
+		fun resolve(path: String) = "$baseAbsolutePath/$path"
 
-        suspend override fun open(path: String): AsyncStream {
-            val raf = RandomAccessFile(File(resolve(path)), "r")
-            return object : AsyncStream() {
-                suspend override fun read(buffer: ByteArray, offset: Int, len: Int): Int = executeInWorker { raf.read(buffer, offset, len) }
-                suspend override fun write(buffer: ByteArray, offset: Int, len: Int) = executeInWorker { raf.write(buffer, offset, len) }
-                suspend override fun setPosition(value: Long) = executeInWorker { raf.seek(value) }
-                suspend override fun getPosition(): Long = executeInWorker { raf.filePointer }
-                suspend override fun setLength(value: Long) = executeInWorker { raf.setLength(value) }
-                suspend override fun getLength(): Long = executeInWorker { raf.length() }
-            }
-        }
+		suspend override fun open(path: String): AsyncStream {
+			val channel = AsynchronousFileChannel.open(Paths.get(path), StandardOpenOption.READ)
 
-        suspend override fun setSize(path: String, size: Long): Unit = executeInWorker {
-            val file = File(resolve(path))
-            FileOutputStream(file, true).channel.use { outChan ->
-                outChan.truncate(size)
-            }
-        }
+			return object : AsyncStream() {
+				var position = 0L
 
-        suspend override fun stat(path: String): VfsStat = executeInWorker {
-            val file = File(resolve(path))
-            VfsStat(
-                    file = VfsFile(this@Impl, "$path/${file.name}"),
-                    exists = file.exists(),
-                    isDirectory = file.isDirectory,
-                    size = file.length()
-            )
-        }
+				suspend override fun read(buffer: ByteArray, offset: Int, len: Int): Int = asyncFun {
+					val bb = ByteBuffer.wrap(buffer, offset, len)
+					val read = completionHandler<Int> { channel.read(bb, position, Unit, it) }
+					position += read
+					read
+				}
 
-        suspend override fun list(path: String) = executeInWorker {
-            asyncGenerate {
-                for (path in Files.newDirectoryStream(Paths.get(resolve(path)))) {
-                    val file = path.toFile()
-                    yield(VfsStat(
-                            file = VfsFile(this@Impl, file.absolutePath.substring(baseAbsolutePath.length + 1)),
-                            exists = file.exists(),
-                            isDirectory = file.isDirectory,
-                            size = file.length()
-                    ))
-                }
-            }
-        }
+				suspend override fun write(buffer: ByteArray, offset: Int, len: Int) = asyncFun {
+					val bb = ByteBuffer.wrap(buffer, offset, len)
+					val write = completionHandler<Int> { channel.write(bb, position, Unit, it) }
+					position += write
+					Unit
+				}
+				suspend override fun setPosition(value: Long) = run { position = value }
+				suspend override fun getPosition(): Long = position
+				suspend override fun setLength(value: Long) {
+					channel.truncate(value)
+				}
+				suspend override fun getLength(): Long = channel.size()
+			}
+		}
 
-        override fun toString(): String = "LocalVfs(${base.absolutePath})"
-    }
-    return Impl().root
+		suspend override fun setSize(path: String, size: Long): Unit = executeInWorker {
+			val file = File(resolve(path))
+			FileOutputStream(file, true).channel.use { outChan ->
+				outChan.truncate(size)
+			}
+		}
+
+		suspend override fun stat(path: String): VfsStat = executeInWorker {
+			val file = File(resolve(path))
+			VfsStat(
+				file = VfsFile(this@Impl, "$path/${file.name}"),
+				exists = file.exists(),
+				isDirectory = file.isDirectory,
+				size = file.length()
+			)
+		}
+
+		suspend override fun list(path: String) = executeInWorker {
+			asyncGenerate {
+				for (path in Files.newDirectoryStream(Paths.get(resolve(path)))) {
+					val file = path.toFile()
+					yield(VfsStat(
+						file = VfsFile(this@Impl, file.absolutePath.substring(baseAbsolutePath.length + 1)),
+						exists = file.exists(),
+						isDirectory = file.isDirectory,
+						size = file.length()
+					))
+				}
+			}
+		}
+
+		override fun toString(): String = "LocalVfs(${base.absolutePath})"
+	}
+	return Impl().root
 }
 
+inline suspend fun <T> completionHandler(crossinline callback: (CompletionHandler<T, Unit>) -> Unit) = suspendCoroutine<T> { c ->
+	callback(object : CompletionHandler<T, Unit> {
+		override fun completed(result: T, attachment: Unit?) {
+			c.resume(result)
+		}
+
+		override fun failed(exc: Throwable, attachment: Unit?) {
+			c.resumeWithException(exc)
+		}
+	})
+}
