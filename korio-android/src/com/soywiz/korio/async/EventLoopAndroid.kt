1751ecff93fce194e1535e8660cb0b38e2e63af2
Carlos Ballesteros Velasco <soywiz@gmail.com>
2017-01-04 20:38:38 +0100

    Fixed Android EventLoop

--- korio-android/src/com/soywiz/korio/async/EventLoopAndroid.kt
+++ korio-android/src/com/soywiz/korio/async/EventLoopAndroid.kt
@@ -1,87 +1,45 @@
 package com.soywiz.korio.async
 
-import com.soywiz.korio.util.compareToChain
-import com.soywiz.korio.util.threadLocal
+import com.soywiz.korio.android.KorioAndroidContext
 import java.io.Closeable
-import java.util.*
-import java.util.concurrent.atomic.AtomicBoolean
 
 class EventLoopAndroid : EventLoop {
-	data class TimerHandler(val time: Long, val handler: () -> Unit, val id: Int)
-
-	var lastId = 0
-	//val handlers = ConcurrentLinkedDeque<() -> Unit>()
-	var timerHandlers = TreeSet<TimerHandler>(Comparator<TimerHandler> { a, b -> if (Objects.equals(a, b)) 0 else a.time.compareTo(b.time).compareToChain { Objects.hash(a).compareTo(Objects.hash(b)) } })
-
-	var eventLoopRunning = AtomicBoolean(false)
-
 	override fun init(): Unit {
-		ensureEventLoop()
 	}
-
-	private fun ensureEventLoop(): Unit {
-		if (!eventLoopRunning.compareAndSet(false, true)) return
-		Thread {
-			while (timerHandlers.isNotEmpty() || tasksInProgress.get() > 0) {
-				val now = System.currentTimeMillis()
-				while (lock { timerHandlers.isNotEmpty() }) {
-					val handler = lock { timerHandlers.first() }
-					if (now >= handler.time) {
-						lock { timerHandlers.remove(handler) }
-						handler.handler()
-					} else {
-						break
-					}
-				}
-				Thread.sleep(16L)
-			}
-			eventLoopRunning.compareAndSet(true, false)
-		}.apply {
-			isDaemon = true
-		}.start()
-	}
-
-	val immediates by threadLocal { LinkedList<() -> Unit>() }
-	var insideImmediate by threadLocal { false }
 
 	override fun setImmediate(handler: () -> Unit) {
-		ensureEventLoop()
-		immediates += handler
-		if (!insideImmediate) {
-			insideImmediate = true
-			try {
-				while (immediates.isNotEmpty()) {
-					val immediate = immediates.removeFirst()
-					immediate()
-				}
-			} finally {
-				insideImmediate = false
-			}
+		KorioAndroidContext.runOnUiThread {
+			handler()
 		}
 	}
 
 	override fun setTimeout(ms: Int, callback: () -> Unit): Closeable {
-		ensureEventLoop()
-		val handler = TimerHandler(System.currentTimeMillis() + ms, callback, lastId++)
-		lock { timerHandlers.add(handler) }
-		return Closeable { lock { timerHandlers.remove(handler) } }
+		var cancelled = false
+		android.os.Handler().postDelayed(Runnable {
+			if (!cancelled) {
+				KorioAndroidContext.runOnUiThread {
+					callback()
+				}
+			}
+		}, ms.toLong())
+		return Closeable { cancelled = true }
 	}
-
 
 	override fun setInterval(ms: Int, callback: () -> Unit): Closeable {
-		ensureEventLoop()
-		var ccallback: (() -> Unit)? = null
-		var disposable: Closeable? = null
-
-		ccallback = {
-			callback()
-			disposable = setTimeout(ms, ccallback!!)
+		var cancelled = false
+		fun step() {
+			android.os.Handler().postDelayed(Runnable {
+				if (!cancelled) {
+					step()
+					KorioAndroidContext.runOnUiThread {
+						callback()
+					}
+				}
+			}, ms.toLong())
 		}
 
-		disposable = setTimeout(ms, ccallback!!)
+		step()
 
-		return Closeable { disposable?.close() }
+		return Closeable { cancelled = true }
 	}
-
-	inline private fun <T> lock(callback: () -> T) = synchronized(this, callback)
 }
--- korio-core/src/com/soywiz/korio/async/Signal.kt
+++ korio-core/src/com/soywiz/korio/async/Signal.kt
@@ -4,11 +4,17 @@ class Signal<T> {
 	internal val handlers = arrayListOf<(T) -> Unit>()
 
 	fun add(handler: (T) -> Unit) {
-		handlers += handler
+		synchronized(handlers) { handlers += handler }
 	}
 
 	operator fun invoke(value: T) {
-		for (handler in handlers) handler.invoke(value)
+		for (handler in synchronized(handlers) { handlers.toList() }) {
+			handler(value)
+		}
+		//while (handlers.isNotEmpty()) {
+		//	val handler = handlers.remove()
+		//	handler.invoke(value)
+		//}
 	}
 
 	operator fun invoke(value: (T) -> Unit) = add(value)
