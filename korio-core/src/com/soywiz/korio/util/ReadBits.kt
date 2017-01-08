10da897be72d63785641e05ff25f7e135fc44e56
Carlos Ballesteros Velasco <soywiz@gmail.com>
2017-01-08 04:58:16 +0100

    Fixed _be readings

--- korio-core/src/com/soywiz/korio/util/ReadBits.kt
+++ korio-core/src/com/soywiz/korio/util/ReadBits.kt
@@ -17,8 +17,8 @@ fun ByteArray.readS16_be(o: Int): Int = (((readU8(o + 0) shl 8) or (readU8(o + 1
 fun ByteArray.readS32_be(o: Int): Int = ((readU8(o + 0) shl 24) or (readU8(o + 1) shl 16) or (readU8(o + 2) shl 8) or (readU8(o + 3) shl 0))
 fun ByteArray.readU16_be(o: Int): Int = (readU8(o + 0) shl 8) or (readU8(o + 1) shl 0)
 fun ByteArray.readU24_be(o: Int): Int = (readU8(o + 0) shl 16) or (readU8(o + 1) shl 8) or (readU8(o + 2) shl 0)
-fun ByteArray.readU32_be(o: Int): Long = readS32_le(o).toLong() and 0xFFFFFFFFL
-fun ByteArray.readS64_be(o: Int): Long = (readU32_le(o + 0) shl 32) or (readU32_le(o + 4) shl 0)
+fun ByteArray.readU32_be(o: Int): Long = readS32_be(o).toLong() and 0xFFFFFFFFL
+fun ByteArray.readS64_be(o: Int): Long = (readU32_be(o + 0) shl 32) or (readU32_be(o + 4) shl 0)
 
 fun ByteArray.readF32_be(o: Int): Float = java.lang.Float.intBitsToFloat(readS32_be(o))
 fun ByteArray.readF64_be(o: Int): Double = java.lang.Double.longBitsToDouble(readS64_be(o))
