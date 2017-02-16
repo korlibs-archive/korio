f79d2308f14c012eaeaabff3d6baec9cded578ef
Carlos Ballesteros Velasco <soywiz@gmail.com>
2017-02-16 18:29:23 +0100

    Some extra improvements

--- korio-core/src/com/soywiz/korio/util/Extra.kt
+++ korio-core/src/com/soywiz/korio/util/Extra.kt
@@ -6,6 +6,24 @@ interface Extra {
 	var extra: HashMap<String, Any?>?
 
 	class Mixin(override var extra: HashMap<String, Any?>? = null) : Extra
+
+	@Suppress("UNCHECKED_CAST", "NOTHING_TO_INLINE")
+	class Property<T : Any?>(val name: String, val defaultGen: () -> T) {
+		inline operator fun getValue(thisRef: Extra, property: KProperty<*>): T {
+			val res = (thisRef.extra?.get(name) as T?)
+			if (res == null) {
+				val r = defaultGen()
+				setValue(thisRef, property, r)
+				return r
+			}
+			return res
+		}
+
+		inline operator fun setValue(thisRef: Extra, property: KProperty<*>, value: T): Unit = run {
+			if (thisRef.extra == null) thisRef.extra = hashMapOf()
+			thisRef.extra?.set(name, value as Any?)
+		}
+	}
 }
 
 @Suppress("UNCHECKED_CAST", "NOTHING_TO_INLINE")
