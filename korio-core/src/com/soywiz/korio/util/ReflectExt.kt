d07ffaf2b1a4bd47d0cff476d399a5287394098e
Carlos Ballesteros Velasco <soywiz@gmail.com>
2017-04-17 00:12:24 +0200

    Added Class.isSubtypeOf extension

--- korio-core/src/com/soywiz/korio/util/ReflectExt.kt
+++ korio-core/src/com/soywiz/korio/util/ReflectExt.kt
@@ -3,3 +3,5 @@ package com.soywiz.korio.util
 import java.lang.reflect.Field
 
 val Class<*>.allDeclaredFields: List<Field> get() = this.declaredFields.toList() + (this.superclass?.allDeclaredFields?.toList() ?: listOf<Field>())
+
+fun Class<*>.isSubtypeOf(that: Class<*>) = that.isAssignableFrom(this)
