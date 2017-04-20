31e03e35dff848eeda72351669b7f9828cee4b0c
Carlos Ballesteros Velasco <soywiz@gmail.com>
2017-04-20 02:02:39 +0200

    Added Extra.PropertyThis

--- korio-core/test/com/soywiz/korio/util/ExtraTest.kt
+++ korio-core/test/com/soywiz/korio/util/ExtraTest.kt
@@ -4,16 +4,20 @@ import org.junit.Assert
 import org.junit.Test
 
 class ExtraTest {
-	class Demo : Extra by Extra.Mixin()
+	class Demo : Extra by Extra.Mixin() {
+		val default = 9
+	}
 
-	var Demo.demo by extraProperty("demo", 0)
+	var Demo.demo by Extra.Property { 0 }
+	var Demo.demo2 by Extra.PropertyThis<Demo, Int> { default }
 
 	@Test
 	fun name() {
 		val demo = Demo()
 		Assert.assertEquals(0, demo.demo)
+		Assert.assertEquals(9, demo.demo2)
 		demo.demo = 7
 		Assert.assertEquals(7, demo.demo)
-		Assert.assertEquals("{demo=7}", demo.extra.toString())
+		Assert.assertEquals("{demo=7, demo2=9}", demo.extra.toString())
 	}
 }
