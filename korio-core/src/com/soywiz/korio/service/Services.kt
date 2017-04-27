fb247dc0521a4e379d0581747db10192dbe75cca
Carlos Ballesteros Velasco <soywiz@gmail.com>
2017-04-27 22:01:23 +0200

    Services facility now allows to register classes and get a list of compatible services LogVfs now tracks modified files

--- korio-core/src/com/soywiz/korio/service/Services.kt
+++ korio-core/src/com/soywiz/korio/service/Services.kt
@@ -9,10 +9,26 @@ object Services {
 		open val priority: Int = 5000
 	}
 
+	private val implsClasses = hashSetOf<Pair<Class<*>, Class<*>>>()
+	private val impls = hashMapOf<Class<out Services.Impl>, ArrayList<Services.Impl>>()
+
+	fun <T : Services.Impl> register(interfase: Class<T>, impl: Class<out T>) {
+		val pair = interfase to impl
+		if (pair !in implsClasses) {
+			implsClasses += pair
+			impls.getOrPut(interfase) { arrayListOf() } += impl.newInstance()
+		}
+	}
+
 	inline fun <reified T : Impl> load(): T = load(T::class.java)
 
-	fun <T : Impl> load(clazz: Class<T>): T = ServiceLoader.load(clazz)
+	fun <T : Impl> load(clazz: Class<T>): T = loadList(clazz).firstOrNull() ?: invalidOp("Can't find implementation for ${clazz.name}")
+
+	fun <T : Impl> loadList(clazz: Class<T>): List<T> {
+		@Suppress("UNCHECKED_CAST")
+		return (((impls[clazz] as? List<T>) ?: listOf()) + ServiceLoader.load(clazz))
 			.filter { it.available }
 			.sortedBy { it.priority }
-			.firstOrNull() ?: invalidOp("Can't find implementation for ${clazz.name}")
+			.toList()
+	}
 }
