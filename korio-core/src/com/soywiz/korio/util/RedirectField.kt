80637319410658c1afbf5e9641476e839b7866a9
Carlos Ballesteros Velasco <soywiz@gmail.com>
2017-04-27 16:17:09 +0200

    Added possibility of redirecting fields with holders that change

--- korio-core/src/com/soywiz/korio/util/RedirectField.kt
+++ korio-core/src/com/soywiz/korio/util/RedirectField.kt
@@ -20,6 +20,18 @@ class RedirectMutableField<V>(val redirect: KMutableProperty0<V>) {
 	}
 }
 
+class RedirectMutableFieldGen<V>(val redirect: () -> KMutableProperty0<V>) {
+	inline operator fun getValue(thisRef: Any, property: KProperty<*>): V {
+		return redirect().get()
+	}
+
+	inline operator fun setValue(thisRef: Any, property: KProperty<*>, value: V) {
+		redirect().set(value)
+	}
+}
+
+inline fun <V> redirectField(noinline redirect: () -> KMutableProperty0<V>) = RedirectMutableFieldGen(redirect)
+
 inline fun <V> redirectField(redirect: KMutableProperty0<V>) = RedirectMutableField(redirect)
 inline fun <V> redirectField(redirect: KProperty0<V>) = RedirectField(redirect)
 
