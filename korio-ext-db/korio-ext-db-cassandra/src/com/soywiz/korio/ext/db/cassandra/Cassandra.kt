dd55a3c02d8b5f10848746081b8aca699c0c02a4
Carlos Ballesteros Velasco <soywiz@gmail.com>
2017-07-02 05:07:21 +0200

    Make Cassandra.Columns and Cassandra.Row to be List and Map

--- korio-ext-db/korio-ext-db-cassandra/src/com/soywiz/korio/ext/db/cassandra/Cassandra.kt
+++ korio-ext-db/korio-ext-db-cassandra/src/com/soywiz/korio/ext/db/cassandra/Cassandra.kt
@@ -146,9 +146,9 @@ class Cassandra private constructor(val s: AsyncStream) {
 		fun interpret(data: ByteArray): Any? = type.interpret(data)
 	}
 
-	data class Columns(val columns: List<Column>) {
-		val size: Int get() = columns.size
-		operator fun get(index: Int) = columns[index]
+	data class Columns(val columns: List<Column>) : List<Column> by columns {
+		fun contains(name: String): Boolean = columns.any { it.name == name }
+
 		fun getIndex(name: String): Int {
 			val index = columns.indexOfFirst { it.name == name }
 			if (index < 0) throw IllegalArgumentException("Can't find column '$name'")
@@ -159,11 +159,27 @@ class Cassandra private constructor(val s: AsyncStream) {
 	data class Row(
 		val columns: Columns,
 		val data: List<ByteArray>
-	) : Collection<ByteArray> by data {
-		operator fun get(name: String) = data[columns.getIndex(name)]
-		operator fun get(index: Int) = data[index]
-		fun getString(index: Int) = this[index].toString(Charsets.UTF_8)
-		fun getString(name: String) = this[name].toString(Charsets.UTF_8)
+	) : Map<String, Any?> {
+		data class MyEntry(override val key: String, override val value: Any?) : Map.Entry<String, Any?>
+		override val size: Int = data.size
+		override val keys: Set<String> by lazy { columns.columns.map { it.name }.toSet() }
+		override val values: List<Any?> by lazy { (0 until size).map { this[it] } }
+		override val entries: Set<Map.Entry<String, Any?>> by lazy { (0 until size).map { MyEntry(columns[it].name, values[it]) }.toSet() }
+		override fun containsKey(key: String): Boolean = keys.contains(key)
+		override fun containsValue(value: Any?): Boolean = values.contains(value)
+
+		override fun isEmpty(): Boolean = size == 0
+
+		fun getColumn(name: String) = columns[columns.getIndex(name)]
+		fun getColumnIndex(name: String) = columns.getIndex(name)
+
+		operator fun get(index: Int) = columns[index].interpret(data[index])
+		fun getRaw(index: Int) = data[index]
+		fun getString(index: Int) = getRaw(index).toString(Charsets.UTF_8)
+
+		override operator fun get(key: String) = get(getColumnIndex(key))
+		fun getRaw(key: String) = getRaw(getColumnIndex(key))
+		fun getString(key: String) = getString(getColumnIndex(key))
 
 		override fun toString(): String = columns.columns.map { "${it.name}=${it.interpret(data[it.index])}" }.joinToString(", ")
 	}
