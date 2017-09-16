9111e76c99c069021186d201945bf282b51ad8a4
soywiz <soywiz@gmail.com>
2017-09-16 22:55:26 +0200

    Use kotlin's suspendCoroutine for now

--- korio-core/src/com/soywiz/korio/coroutine/coroutine.kt
+++ korio-core/src/com/soywiz/korio/coroutine/coroutine.kt
@@ -6,6 +6,7 @@ import com.soywiz.korio.async.toEventLoop
 import kotlin.coroutines.experimental.Continuation
 import kotlin.coroutines.experimental.createCoroutine
 import kotlin.coroutines.experimental.startCoroutine
+import kotlin.coroutines.experimental.suspendCoroutine
 
 //val COROUTINE_SUSPENDED = kotlin.coroutines.experimental.COROUTINE_SUSPENDED
 val COROUTINE_SUSPENDED = kotlin.coroutines.experimental.intrinsics.COROUTINE_SUSPENDED
@@ -41,11 +42,16 @@ inline suspend fun <T> korioSuspendCoroutine(crossinline block: (Continuation<T>
 }
 
 inline suspend fun <T> _korioSuspendCoroutine(crossinline block: (Continuation<T>) -> Unit): T {
-	return kotlin.coroutines.experimental.intrinsics.suspendCoroutineOrReturn { c: Continuation<T> ->
-		val unsafe = UnsafeContinuation(c)
-		block(unsafe)
-		unsafe.getResult()
+	return suspendCoroutine<T> { c: Continuation<T> ->
+		block(c)
 	}
+
+	// @TODO: Enable just in jtransc that is single-threaded as an optimization?
+	//return kotlin.coroutines.experimental.intrinsics.suspendCoroutineOrReturn { c: Continuation<T> ->
+	//	val unsafe = UnsafeContinuation(c)
+	//	block(unsafe)
+	//	unsafe.getResult()
+	//}
 }
 
 fun <R, T> (suspend R.() -> T).korioStartCoroutine(receiver: R, completion: Continuation<T>) = this.startCoroutine(receiver, completion)
@@ -53,58 +59,58 @@ fun <T> (suspend () -> T).korioStartCoroutine(completion: Continuation<T>) = thi
 fun <T> (suspend () -> T).korioCreateCoroutine(completion: Continuation<T>): Continuation<Unit> = this.createCoroutine(completion)
 fun <R, T> (suspend R.() -> T).korioCreateCoroutine(receiver: R, completion: Continuation<T>): Continuation<Unit> = this.createCoroutine(receiver, completion)
 
-private val UNDECIDED: Any? = Any()
-private val RESUMED: Any? = Any()
+//private val UNDECIDED: Any? = Any()
+//private val RESUMED: Any? = Any()
+//
+//private class Fail(val exception: Throwable)
 
-private class Fail(val exception: Throwable)
-
-@PublishedApi
-internal class UnsafeContinuation<in T> @PublishedApi internal constructor(private val delegate: Continuation<T>) : Continuation<T> {
-	override val context: CoroutineContext get() = delegate.context
-
-	@Volatile
-	private var result: Any? = UNDECIDED
-
-	override fun resume(value: T) {
-		val result = this.result
-		when {
-			result === UNDECIDED -> {
-				this.result = value
-			}
-			result === COROUTINE_SUSPENDED -> {
-				this.result = RESUMED
-				delegate.resume(value)
-			}
-			else -> throw java.lang.IllegalStateException("Already resumed")
-		}
-	}
-
-	override fun resumeWithException(exception: Throwable) {
-		val result = this.result
-		when {
-			result === UNDECIDED -> {
-				this.result = Fail(exception)
-			}
-			result === kotlin.coroutines.experimental.intrinsics.COROUTINE_SUSPENDED -> {
-				this.result = RESUMED
-				delegate.resumeWithException(exception)
-				return
-			}
-			else -> throw java.lang.IllegalStateException("Already resumed")
-		}
-	}
-
-	@PublishedApi
-	internal fun getResult(): Any? {
-		val result = this.result
-		if (result === UNDECIDED) {
-			this.result = kotlin.coroutines.experimental.intrinsics.COROUTINE_SUSPENDED
-			return kotlin.coroutines.experimental.intrinsics.COROUTINE_SUSPENDED
-		}
-		when {
-			result === RESUMED -> return kotlin.coroutines.experimental.intrinsics.COROUTINE_SUSPENDED // already called continuation, indicate SUSPENDED_MARKER upstream
-			result is Fail -> throw result.exception
-			else -> return result // either SUSPENDED_MARKER or data
-		}
-	}
-}
+//@PublishedApi
+//internal class UnsafeContinuation<in T> @PublishedApi internal constructor(private val delegate: Continuation<T>) : Continuation<T> {
+//	override val context: CoroutineContext get() = delegate.context
+//
+//	@Volatile
+//	private var result: Any? = UNDECIDED
+//
+//	override fun resume(value: T) {
+//		val result = this.result
+//		when {
+//			result === UNDECIDED -> {
+//				this.result = value
+//			}
+//			result === COROUTINE_SUSPENDED -> {
+//				this.result = RESUMED
+//				delegate.resume(value)
+//			}
+//			else -> throw java.lang.IllegalStateException("Already resumed")
+//		}
+//	}
+//
+//	override fun resumeWithException(exception: Throwable) {
+//		val result = this.result
+//		when {
+//			result === UNDECIDED -> {
+//				this.result = Fail(exception)
+//			}
+//			result === kotlin.coroutines.experimental.intrinsics.COROUTINE_SUSPENDED -> {
+//				this.result = RESUMED
+//				delegate.resumeWithException(exception)
+//				return
+//			}
+//			else -> throw java.lang.IllegalStateException("Already resumed")
+//		}
+//	}
+//
+//	@PublishedApi
+//	internal fun getResult(): Any? {
+//		val result = this.result
+//		if (result === UNDECIDED) {
+//			this.result = kotlin.coroutines.experimental.intrinsics.COROUTINE_SUSPENDED
+//			return kotlin.coroutines.experimental.intrinsics.COROUTINE_SUSPENDED
+//		}
+//		when {
+//			result === RESUMED -> return kotlin.coroutines.experimental.intrinsics.COROUTINE_SUSPENDED // already called continuation, indicate SUSPENDED_MARKER upstream
+//			result is Fail -> throw result.exception
+//			else -> return result // either SUSPENDED_MARKER or data
+//		}
+//	}
+//}
