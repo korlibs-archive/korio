348422c2cae283e354f425604895a82ca11c94e1
soywiz <soywiz@gmail.com>
2017-09-17 13:43:28 +0200

    Fix MimeType

--- korio-core/src/com/soywiz/korio/vfs/MimeType.kt
+++ korio-core/src/com/soywiz/korio/vfs/MimeType.kt
@@ -1,15 +1,28 @@
 package com.soywiz.korio.vfs
 
+import com.soywiz.korio.util.allDeclaredFields
+import java.lang.reflect.Modifier
+
 class MimeType(val mime: String, val exts: List<String>) : Vfs.Attribute {
 	companion object {
+		@JvmStatic
 		val APPLICATION_OCTET_STREAM = MimeType("application/octet-stream", listOf("bin"))
+		@JvmStatic
 		val APPLICATION_JSON = MimeType("application/json", listOf("json"))
-		val IMAGE_GIF = MimeType("image/gif", listOf("gif"))
-		val IMAGE_JPEG = MimeType("image/jpeg", listOf("jpg", "jpeg"))
+		@JvmStatic
 		val IMAGE_PNG = MimeType("image/png", listOf("png"))
+		@JvmStatic
+		val IMAGE_JPEG = MimeType("image/jpeg", listOf("jpg", "jpeg"))
+		@JvmStatic
+		val IMAGE_GIF = MimeType("image/gif", listOf("gif"))
+		@JvmStatic
 		val TEXT_HTML = MimeType("text/html", listOf("htm", "html"))
+		@JvmStatic
 		val TEXT_PLAIN = MimeType("text/plain", listOf("txt", "text"))
-		val TEXT_CSS = MimeType("text/ss", listOf("css"))
+		@JvmStatic
+		val TEXT_CSS = MimeType("text/css", listOf("css"))
+		@JvmStatic
+		val TEXT_JS = MimeType("application/javascript", listOf("js"))
 
 		private val byExtensions = hashMapOf<String, MimeType>()
 
@@ -22,12 +35,11 @@ class MimeType(val mime: String, val exts: List<String>) : Vfs.Attribute {
 		}
 
 		init {
-			register(APPLICATION_OCTET_STREAM)
-			register(TEXT_PLAIN)
-			register(TEXT_CSS)
-			register(APPLICATION_JSON)
-			register(IMAGE_JPEG)
-			register(IMAGE_PNG)
+			for (field in MimeType::class.java.allDeclaredFields) {
+				if (Modifier.isStatic(field.modifiers) && (field.type == MimeType::class.java)) {
+					register(field.get(null) as MimeType)
+				}
+			}
 		}
 
 		fun getByExtension(ext: String, default: MimeType = APPLICATION_OCTET_STREAM): MimeType = byExtensions[ext.toLowerCase()] ?: default
