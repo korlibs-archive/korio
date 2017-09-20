d3b74c9079550e8cf841135581dbb8dc747ceafb
Carlos Ballesteros Velasco <soywiz@gmail.com>
2017-09-21 00:20:51 +0200

    Multiplatform: Some more work

--- korio-test/src/com/soywiz/korio/junit/TestTools.kt
+++
@@ -1,8 +0,0 @@
-package com.soywiz.korio.junit
-
-import com.soywiz.korio.async.EventLoopTest
-
-fun syncTest(block: suspend EventLoopTest.() -> Unit): Unit {
-	//sync(el = EventLoopTest(), step = 10, block = block)
-	TODO()
-}
