ce0ebbea16082061f88df0c0c155a658f065de65
Carlos Ballesteros Velasco <soywiz@gmail.com>
2017-09-25 16:01:30 +0200

    Multiproject: URL fixes

--- korio-common/src/main/kotlin/com/soywiz/korio/lang/URL.kt
+++ korio-common/src/main/kotlin/com/soywiz/korio/lang/URL.kt
@@ -2,34 +2,74 @@ package com.soywiz.korio.lang
 
 import com.soywiz.korio.vfs.VfsUtil
 
-class URL(val url: String) {
-	private val partsA = url.split("://", limit = 2)
-	private val partsB = partsA.getOrElse(1) { "" }.split("/", limit = 2)
-	private val partsC = partsB.getOrElse(1) { "" }.split("?", limit = 2)
-	private val fullHost = partsB.getOrElse(0) { "" }
-	private val fullPath = partsC.getOrElse(0) { "" }
-	private val queryString = partsC.getOrElse(1) { "" }
+data class URL private constructor(
+	val dummy: Boolean,
+	val protocol: String,
+	val auth: String,
+	val host: String,
+	val path: String,
+	val query: String
+) {
+	val user: String get() = auth.split(':').getOrElse(0) { "" }
+	val password: String get() = auth.split(':').getOrElse(1) { "" }
 
-	private val fullHostParts = fullHost.split("@", limit = 2)
-	private val userPass = if (fullHostParts.size == 2) fullHostParts[0] else ""
-	private val userPassParts = userPass.split(':', limit = 2)
+	private val str: String by lazy {
+		val out = StringBuilder()
+		out.append("$protocol://")
+		if (auth.isNotEmpty()) out.append("$auth@")
+		out.append(host)
+		out.append('/')
+		out.append(path.trimStart('/'))
+		if (query.isNotEmpty()) out.append("?$query")
+		out.toString()
+	}
 
-	val protocol: String = partsA.getOrElse(0) { "" }
-	val user = userPassParts.getOrElse(0) { "" }
-	val password = userPassParts.getOrElse(1) { "" }
-	val host = fullHostParts.last()
-	val path = "/" + fullPath
-	val query = queryString
+	override fun toString(): String = str
 
-	override fun toString(): String = url
+	//fun withProtocol(protocol: String) = copy(protocol = protocol)
+	//fun withHost(host: String) = copy(host = host)
+	//fun withPath(path: String) = copy(path = path)
+	//fun withQuery(query: String) = copy(query = query)
+	//fun withAuth(auth: String) = copy(auth = auth)
 
+	companion object {
+		operator fun invoke(
+			protocol: String,
+			auth: String,
+			host: String,
+			path: String,
+			query: String
+		): URL = URL(false, protocol, auth, host, path, query)
+
+		operator fun invoke(url: String): URL {
+			val partsA = url.split("://", limit = 2)
+			val partsB = partsA.getOrElse(1) { "" }.split("/", limit = 2)
+			val partsC = partsB.getOrElse(1) { "" }.split("?", limit = 2)
+			val fullHost = partsB.getOrElse(0) { "" }
+			val fullPath = partsC.getOrElse(0) { "" }
+			val queryString = partsC.getOrElse(1) { "" }
+
+			val fullHostParts = fullHost.split("@", limit = 2)
+			val userPassStr = if (fullHostParts.size == 2) fullHostParts[0] else ""
+
+			return URL(
+				protocol = partsA.getOrElse(0) { "" },
+				auth = userPassStr,
+				host = fullHostParts.last(),
+				path = "/" + fullPath.trimStart('/'),
+				query = queryString
+			)
+		}
+	}
 }
 
 object URIUtils {
 	fun isAbsolute(base: String): Boolean = base.contains("://")
+
 	fun resolve(base: String, access: String): String = if (isAbsolute(access)) {
 		access
 	} else {
-		VfsUtil.combine(base, access)
+		val url = URL(base + "/" + access)
+		url.copy(path = VfsUtil.normalize(url.path)).toString()
 	}
 }
