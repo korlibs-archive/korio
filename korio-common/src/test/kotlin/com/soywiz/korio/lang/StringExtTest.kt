d7ba7bf7aa6530b6dc7bbea57645153a330823a9
soywiz <soywiz@gmail.com>
2017-10-07 15:17:20 +0200

    Fixed String.format without size

--- korio-common/src/test/kotlin/com/soywiz/korio/lang/StringExtTest.kt
+++ korio-common/src/test/kotlin/com/soywiz/korio/lang/StringExtTest.kt
@@ -12,4 +12,11 @@ class StringExtTest {
 		assertEquals(listOf("a", "90", "b", "3"), "a90b3".splitKeep(Regex("\\d+")))
 		assertEquals(listOf("a", "90", "b", "3", "cc"), "a90b3cc".splitKeep(Regex("\\d+")))
 	}
+
+	@Test
+	fun format() {
+		assertEquals("hello 10", "%s %d".format("hello", 10))
+		assertEquals("ff", "%02x".format(255))
+		assertEquals("FF", "%02X".format(255))
+	}
 }
