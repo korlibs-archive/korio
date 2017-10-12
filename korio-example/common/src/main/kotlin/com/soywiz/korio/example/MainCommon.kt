d91e7fb04946244c14969278fb11440f76ebd2f6
soywiz <soywiz@gmail.com>
2017-10-12 11:12:13 +0200

    Normalized directory structure

--- korio-example/common/src/main/kotlin/com/soywiz/korio/example/MainCommon.kt
+++ korio-example/common/src/main/kotlin/com/soywiz/korio/example/MainCommon.kt
@@ -2,12 +2,16 @@ package com.soywiz.korio.example
 
 import com.soywiz.korio.Korio
 import com.soywiz.korio.net.http.createHttpClient
+import com.soywiz.korio.util.OS
 
 object MainCommon {
 	fun main() = Korio {
 		println("Hello from MainCommon.main!")
 		val client = createHttpClient()
-		val indexHtmlContent = client.readString("index.html")
+		//val indexHtmlContent = client.readString("index.html")
+		val indexHtmlContent = client.readString(
+			if (OS.isJs) "index.html" else "https://www.google.es/"
+		)
 		println(indexHtmlContent.length)
 		println(indexHtmlContent)
 		println(indexHtmlContent.toList())
