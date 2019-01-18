07361b38af935dfaa770ab9c1dbec8232b577249
soywiz <soywiz@gmail.com>
2019-01-18 11:32:03 +0100

    Handle Future.await thrown state

--- src/nativeCommonMain/kotlin/com/soywiz/korio/async/FutureExt.kt
+++ src/nativeCommonMain/kotlin/com/soywiz/korio/async/FutureExt.kt
@@ -3,9 +3,12 @@ package com.soywiz.korio.async
 suspend fun <T> kotlin.native.concurrent.Future<T>.await(): T {
 	var n = 0
 	while (this.state != kotlin.native.concurrent.FutureState.COMPUTED) {
-		if (this.state == kotlin.native.concurrent.FutureState.INVALID) error("Error in worker")
-		if (this.state == kotlin.native.concurrent.FutureState.CANCELLED) kotlinx.coroutines.CancellationException("cancelled")
-		kotlinx.coroutines.delay(((n++).toDouble() / 3.0).toLong())
+		when (this.state) {
+			kotlin.native.concurrent.FutureState.INVALID -> error("Error in worker")
+			kotlin.native.concurrent.FutureState.CANCELLED -> kotlinx.coroutines.CancellationException("cancelled")
+			kotlin.native.concurrent.FutureState.THROWN -> error("Worker thrown exception")
+			else -> kotlinx.coroutines.delay(((n++).toDouble() / 3.0).toLong())
+		}
 	}
 	return this.result
 }
