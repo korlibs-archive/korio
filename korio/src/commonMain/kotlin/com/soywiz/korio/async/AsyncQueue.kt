46705f4fa0a1217e61433c8467e54b452074b78e
soywiz <soywiz@gmail.com>
2019-04-28 04:23:28 +0200

    Some additions

--- korio/src/commonMain/kotlin/com/soywiz/korio/async/AsyncQueue.kt
+++ korio/src/commonMain/kotlin/com/soywiz/korio/async/AsyncQueue.kt
@@ -1,7 +1,13 @@
 package com.soywiz.korio.async
 
+import com.soywiz.korio.concurrent.lock.*
+import com.soywiz.korio.concurrent.lock.Lock
 import kotlinx.coroutines.*
 import kotlin.coroutines.*
+
+interface AsyncInvokable {
+	suspend operator fun <T> invoke(func: suspend () -> T): T
+}
 
 //class AsyncQueue(val context: CoroutineContext) {
 class AsyncQueue {
@@ -39,7 +45,7 @@ class AsyncQueueWithContext(val queue: AsyncQueue, val context: CoroutineContext
 	suspend fun await() = queue.await()
 }
 
-class AsyncThread {
+class AsyncThread : AsyncInvokable {
 	private var lastPromise: Deferred<*> = CompletableDeferred(Unit).apply {
 		this.complete(Unit)
 	}
@@ -65,7 +71,7 @@ class AsyncThread {
 
 	suspend fun <T> queue(func: suspend () -> T): T = invoke(func)
 
-	suspend operator fun <T> invoke(func: suspend () -> T): T {
+	override suspend operator fun <T> invoke(func: suspend () -> T): T {
 		val task = sync(coroutineContext, func)
 		try {
 			val res = task.await()
@@ -86,6 +92,78 @@ class AsyncThread {
 		lastPromise = promise
 		return promise
 
+	}
+}
+
+/**
+ * Creates a queue of processes that will be executed one after another by effectively preventing from executing
+ * them at the same time.
+ * This class is thread-safe.
+ */
+class AsyncThread2 : AsyncInvokable {
+	private val lock = Lock()
+	private var lastPromise: Deferred<*> = CompletableDeferred(Unit)
+
+	suspend fun await() {
+		while (true) {
+			val cpromise = lock { lastPromise }
+			cpromise.await()
+			if (lock { cpromise == lastPromise }) break
+		}
+	}
+
+	fun cancel() = apply {
+		lock { lastPromise }.cancel()
+		lock { lastPromise = CompletableDeferred(Unit) }
+	}
+
+	override suspend operator fun <T> invoke(func: suspend () -> T): T {
+		val task = invoke(coroutineContext, func)
+		try {
+			val res = task.await()
+			return res
+		} catch (e: Throwable) {
+			throw e
+		}
+	}
+
+	private operator fun <T> invoke(context: CoroutineContext, func: suspend () -> T): Deferred<T> = lock {
+		val oldPromise = lastPromise
+		CoroutineScope(context).async {
+			oldPromise.await()
+			func()
+		}.also { lastPromise = it }
+	}
+}
+
+/**
+ * Prevents a named invoke to happen at the same time (by effectively enqueuing by name).
+ * This class is thread-safe.
+ */
+class NamedAsyncThreads(val threadFactory: () -> AsyncInvokable = { AsyncThread2() }) {
+	class AsyncJob(val thread: AsyncInvokable) {
+		var count = 0
+	}
+	private val lock = Lock()
+	private val jobs = LinkedHashMap<String, AsyncJob>()
+
+	internal fun threadsCount() = jobs.size
+
+	suspend operator fun <T> invoke(name: String, func: suspend () -> T): T {
+		val job = lock {
+			jobs.getOrPut(name) { AsyncJob(threadFactory()) }.also { it.count++ }
+		}
+		try {
+			return job.thread.invoke(func)
+		} finally {
+			// Synchronization to prevent another thread from being added in the mean time, or a process queued.
+			lock {
+				job.count--
+				if (job.count == 0) {
+					jobs.remove(name)
+				}
+			}
+		}
 	}
 }
 
