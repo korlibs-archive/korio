d5d51a635cb17c7715ec2a055eeeca699cfe32ef
soywiz <soywiz@gmail.com>
2019-08-22 18:06:29 +0200

    More work

--- @old/buildSrc/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-plugins {
-	id("org.jetbrains.kotlin.jvm").version("1.3.20")
-}
-
-repositories {
-	mavenLocal()
-	jcenter()
-	mavenCentral()
-}
--- @old/buildSrc/src/main/kotlin/Functionality.kt
+++
@@ -1,5 +0,0 @@
-import java.io.*
-import org.gradle.api.*
-
-val Project.hasAndroid get() =
-	(System.getProperty("sdk.dir") != null) || (System.getenv("ANDROID_HOME") != null) || (File(rootDir, "local.properties").exists())
