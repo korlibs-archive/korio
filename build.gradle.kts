d760530960130fb24b7329f51eb15ccce8b5d0eb
soywiz <soywiz@gmail.com>
2020-03-07 12:23:11 +0100

    easy-kotlin-mpp-gradle-plugin 0.7.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.soywiz.korlibs:easy-kotlin-mpp-gradle-plugin:0.7.2") // Kotlin 1.3.70: https://github.com/korlibs/easy-kotlin-mpp-gradle-plugin
+        classpath("com.soywiz.korlibs:easy-kotlin-mpp-gradle-plugin:0.7.3") // Kotlin 1.3.70: https://github.com/korlibs/easy-kotlin-mpp-gradle-plugin
     }
 }
 
