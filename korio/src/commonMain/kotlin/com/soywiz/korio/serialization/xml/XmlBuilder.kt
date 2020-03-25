29ae1dbde69012b50fe26b6941a180d98a79b3c9
soywiz <soywiz@gmail.com>
2020-03-25 01:55:46 +0100

    Added buildXml extension

---
+++ korio/src/commonMain/kotlin/com/soywiz/korio/serialization/xml/XmlBuilder.kt
@@ -0,0 +1,14 @@
+package com.soywiz.korio.serialization.xml
+
+class XmlBuilder internal constructor() {
+    private val nodes = arrayListOf<Xml>()
+    fun node(node: Xml) = node.also { nodes += node }
+    fun node(tag: String, vararg props: Pair<String, Any?>, block: XmlBuilder.() -> Unit = {}): Xml =
+        Xml.Tag(tag, props.filter { it.second != null }.toMap(), XmlBuilder().apply(block).nodes).also { nodes += it }
+    fun comment(comment: String): Xml = Xml.Comment(comment).also { nodes += it }
+    fun text(text: String): Xml = Xml.Text(text).also { nodes += it }
+    fun cdata(text: String): Xml = Xml.Text(text).also { nodes += it }
+}
+
+fun buildXml(rootTag: String, vararg props: Pair<String, Any?>, block: XmlBuilder.() -> Unit): Xml =
+    XmlBuilder().node(rootTag, *props, block = block)
