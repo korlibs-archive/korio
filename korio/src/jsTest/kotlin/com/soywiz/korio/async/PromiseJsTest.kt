fa2d6c7dea86f03730377c6f1d3dd5ab7d769604
Carlos Ballesteros Velasco <soywiz@gmail.com>
2020-05-04 22:04:34 +0200

    Assert that Promise is an actual js Promise on JS

---
+++ korio/src/jsTest/kotlin/com/soywiz/korio/async/PromiseJsTest.kt
@@ -0,0 +1,21 @@
+package com.soywiz.korio.async
+
+import com.soywiz.klock.*
+import com.soywiz.korio.*
+import kotlin.browser.*
+import kotlin.test.*
+
+class PromiseJsTest {
+    @Test
+    fun test() = suspendTest {
+        val startTime = DateTime.now()
+        val value = delay(100)
+        assertTrue(js("(value instanceof Promise)"))
+        assertTrue(js("(typeof value.then == \"function\")"))
+        value.await()
+        val endTime = DateTime.now()
+        assertEquals(true, endTime - startTime >= 100.milliseconds)
+    }
+
+    fun delay(ms: Int): Promise<Unit> = Promise { resolve, reject -> global.setTimeout({ resolve(Unit) }, ms) }
+}
