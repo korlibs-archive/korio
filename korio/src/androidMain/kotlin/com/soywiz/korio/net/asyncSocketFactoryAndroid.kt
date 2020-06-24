f05d2896966ba396decee6f18fde86f8942a482c
Carlos Ballesteros Velasco <soywiz@gmail.com>
2020-06-24 20:54:08 +0200

    Fixes #78: Support SSL on JVM targets (#94)
    * Fixes #78: Support SSL on JVM targets
    
    * Fixes

--- korio/src/androidMain/kotlin/com/soywiz/korio/net/asyncSocketFactoryAndroid.kt
+++ korio/src/androidMain/kotlin/com/soywiz/korio/net/asyncSocketFactoryAndroid.kt
@@ -5,86 +5,67 @@ import com.soywiz.korio.concurrent.atomic.*
 import kotlinx.coroutines.*
 import java.io.*
 import java.net.*
-import java.nio.*
-import java.nio.channels.*
-import java.nio.channels.CompletionHandler
-import java.util.concurrent.*
-import kotlin.coroutines.*
+import javax.net.ssl.SSLServerSocketFactory
+import javax.net.ssl.SSLSocketFactory
 
 internal actual val asyncSocketFactory: AsyncSocketFactory by lazy {
 	object : AsyncSocketFactory() {
-		override suspend fun createClient(secure: Boolean): AsyncClient = JvmAsyncClient()
+		override suspend fun createClient(secure: Boolean): AsyncClient = JvmAsyncClient(secure = secure)
 		override suspend fun createServer(port: Int, host: String, backlog: Int, secure: Boolean): AsyncServer =
-			JvmAsyncServer(port, host, backlog).apply { init() }
+			JvmAsyncServer(port, host, backlog, secure = secure).apply { init() }
 	}
 }
 
-//private val newPool by lazy { Executors.newFixedThreadPool(1) }
-//private val group by lazy { AsynchronousChannelGroup.withThreadPool(newPool) }
+@PublishedApi
+internal val socketDispatcher get() = Dispatchers.IO
+private suspend inline fun <T> doIo(crossinline block: () -> T): T = withContext(socketDispatcher) { block() }
 
-class JvmAsyncClient(private var socket: Socket? = null) : AsyncClient {
-	private val readQueue = AsyncThread()
-	private val writeQueue = AsyncThread()
+class JvmAsyncClient(private var socket: Socket? = null, val secure: Boolean = false) : AsyncClient {
+    private val readQueue = AsyncThread()
+    private val writeQueue = AsyncThread()
 
-	private var socketIs: InputStream? = null
-	private var socketOs: OutputStream? = null
+    private var socketIs: InputStream? = null
+    private var socketOs: OutputStream? = null
 
-	//suspend override fun connect(host: String, port: Int): Unit = suspendCoroutineEL { c ->
-	override suspend fun connect(host: String, port: Int) {
-		withContext(Dispatchers.IO) {
-			socket = Socket(host, port)
-			socketIs = socket?.getInputStream()
-			socketOs = socket?.getOutputStream()
-		}
-	}
+    init {
+        setStreams()
+    }
 
-	override val connected: Boolean get() = socket?.isConnected ?: false
+    private fun setStreams() {
+        socketIs = socket?.getInputStream()
+        socketOs = socket?.getOutputStream()
+    }
 
-	override suspend fun read(buffer: ByteArray, offset: Int, len: Int): Int = readQueue { _read(buffer, offset, len) }
-	//suspend override fun read(buffer: ByteArray, offset: Int, len: Int): Int = _read(buffer, offset, len)
+    override suspend fun connect(host: String, port: Int): Unit = doIo {
+        socket = if (secure) SSLSocketFactory.getDefault().createSocket(host, port) else Socket(host, port)
+        setStreams()
+    }
 
-	override suspend fun write(buffer: ByteArray, offset: Int, len: Int): Unit = writeQueue {
-		_write(buffer, offset, len)
-	}
+    override val connected: Boolean get() = socket?.isConnected ?: false
 
-	//suspend private fun _read(buffer: ByteArray, offset: Int, len: Int): Int = suspendCoroutineEL { c ->
-	private suspend fun _read(buffer: ByteArray, offset: Int, len: Int): Int = withContext(Dispatchers.IO) {
-		socketIs?.read(buffer, offset, len) ?: 0
-	}
+    override suspend fun read(buffer: ByteArray, offset: Int, len: Int): Int = readQueue { doIo { socketIs?.read(buffer, offset, len) ?: -1 } }
+    override suspend fun write(buffer: ByteArray, offset: Int, len: Int): Unit = writeQueue { doIo { socketOs?.write(buffer, offset, len) }.let { Unit } }
 
-	private suspend fun _write(buffer: ByteArray, offset: Int, len: Int): Unit {
-		withContext(Dispatchers.IO) {
-			socketOs?.write(buffer, offset, len)
-		}
-	}
-
-	override suspend fun close() {
-		withContext(Dispatchers.IO) {
-			socket?.close()
-			socketIs?.close()
-			socketOs?.close()
-			socket = null
-			socketIs = null
-			socketOs = null
-		}
-	}
+    override suspend fun close() {
+        doIo {
+            socket?.close()
+            socketIs?.close()
+            socketOs?.close()
+            socket = null
+            socketIs = null
+            socketOs = null
+        }
+    }
 }
 
-class JvmAsyncServer(override val requestPort: Int, override val host: String, override val backlog: Int = -1) :
-	AsyncServer {
-	val ssc = ServerSocket()
-
-	suspend fun init(): Unit {
-		withContext(Dispatchers.IO) {
-			ssc.bind(InetSocketAddress(host, requestPort), backlog)
-		}
-	}
-
-	override val port: Int get() = (ssc.localSocketAddress as? InetSocketAddress)?.port ?: -1
-
-	override suspend fun accept(): AsyncClient {
-		return withContext(Dispatchers.IO) {
-			JvmAsyncClient(ssc.accept())
-		}
-	}
+class JvmAsyncServer(
+    override val requestPort: Int,
+    override val host: String,
+    override val backlog: Int = -1,
+    val secure: Boolean = false
+) : AsyncServer {
+    val ssc = if (secure) SSLServerSocketFactory.getDefault().createServerSocket() else ServerSocket()
+    suspend fun init() = doIo { ssc.bind(InetSocketAddress(host, requestPort), backlog) }
+    override val port: Int get() = (ssc.localSocketAddress as? InetSocketAddress)?.port ?: -1
+    override suspend fun accept(): AsyncClient = JvmAsyncClient(doIo { ssc.accept() })
 }
