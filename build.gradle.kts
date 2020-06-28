f633ec016af6c3368ed200b475131c39dd8bc334
soywiz <soywiz@gmail.com>
2020-06-28 23:44:30 +0200

    More work

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,11 +6,12 @@ buildscript {
         google()
         maven { url = uri("https://dl.bintray.com/korlibs/korlibs/") }
         maven { url = uri("https://plugins.gradle.org/m2/") }
-        //maven { url = uri("https://dl.bintray.com/kotlin/kotlin-dev") }
+        maven { url = uri("https://dl.bintray.com/kotlin/kotlin-eap") }
+        maven { url = uri("https://dl.bintray.com/kotlin/kotlin-dev") }
     }
 
     dependencies {
-        classpath("com.soywiz.korlibs:easy-kotlin-mpp-gradle-plugin:0.8.6") // Kotlin 1.3.72: https://github.com/korlibs/easy-kotlin-mpp-gradle-plugin
+        classpath("com.soywiz.korlibs:easy-kotlin-mpp-gradle-plugin:0.9.3") // Kotlin 1.3.72: https://github.com/korlibs/easy-kotlin-mpp-gradle-plugin
     }
 }
 
