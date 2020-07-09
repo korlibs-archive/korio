7176d586afd34a821db528df017fdc502035cd56
soywiz <soywiz@gmail.com>
2020-07-09 17:57:45 +0200

    Fixes surrogate pairs encoding/decoding

--- korio/src/commonMain/kotlin/com/soywiz/korio/lang/Charset.kt
+++ korio/src/commonMain/kotlin/com/soywiz/korio/lang/Charset.kt
@@ -12,6 +12,40 @@ abstract class Charset(val name: String) {
 		fun forName(name: String): Charset {
 			return UTF8
 		}
+
+        fun StringBuilder.appendCodePoint(codePoint: Int) {
+            if (codePoint in 0xD800..0xDFFF || codePoint > 0xFFFF) {
+                val U0 = codePoint - 0x10000
+                val hs = U0.extract(10, 10)
+                val ls = U0.extract(0, 10)
+                append(((0b110110 shl 10) or (hs)).toChar())
+                append(((0b110111 shl 10) or (ls)).toChar())
+            } else {
+                append(codePoint.toChar())
+            }
+        }
+
+        inline fun decodeCodePoints(src: CharSequence, start: Int, end: Int, block: (codePoint: Int) -> Unit) {
+            var highSurrogate = 0
+            loop@for (n in start until end) {
+                val char = src[n].toInt()
+                val codePoint = if (char in 0xD800..0xDFFF) {
+                    when (char.extract(10, 6)) {
+                        0b110110 -> {
+                            highSurrogate = char and 0x3FF
+                            continue@loop
+                        }
+                        0b110111 -> {
+                            0x10000 + ((highSurrogate shl 10) or (char and 0x3FF))
+                        }
+                        else -> error("Unknown $char")
+                    }
+                } else {
+                    char
+                }
+                block(codePoint)
+            }
+        }
 	}
 }
 
@@ -19,28 +53,26 @@ open class UTC8CharsetBase(name: String) : Charset(name) {
 	private fun createByte(codePoint: Int, shift: Int): Int = codePoint shr shift and 0x3F or 0x80
 
 	override fun encode(out: ByteArrayBuilder, src: CharSequence, start: Int, end: Int) {
-		for (n in start until end) {
-			val codePoint = src[n].toInt()
-
-			if (codePoint and 0x7F.inv() == 0) { // 1-byte sequence
-				out.append(codePoint.toByte())
-			} else {
-				when {
-					codePoint and 0x7FF.inv() == 0 -> // 2-byte sequence
-						out.append((codePoint shr 6 and 0x1F or 0xC0).toByte())
-					codePoint and 0xFFFF.inv() == 0 -> { // 3-byte sequence
-						out.append((codePoint shr 12 and 0x0F or 0xE0).toByte())
-						out.append((createByte(codePoint, 6)).toByte())
-					}
-					codePoint and -0x200000 == 0 -> { // 4-byte sequence
-						out.append((codePoint shr 18 and 0x07 or 0xF0).toByte())
-						out.append((createByte(codePoint, 12)).toByte())
-						out.append((createByte(codePoint, 6)).toByte())
-					}
-				}
-				out.append((codePoint and 0x3F or 0x80).toByte())
-			}
-		}
+        decodeCodePoints(src, start, end) { codePoint ->
+            if (codePoint and 0x7F.inv() == 0) { // 1-byte sequence
+                out.append(codePoint.toByte())
+            } else {
+                when {
+                    codePoint and 0x7FF.inv() == 0 -> // 2-byte sequence
+                        out.append((codePoint shr 6 and 0x1F or 0xC0).toByte())
+                    codePoint and 0xFFFF.inv() == 0 -> { // 3-byte sequence
+                        out.append((codePoint shr 12 and 0x0F or 0xE0).toByte())
+                        out.append((createByte(codePoint, 6)).toByte())
+                    }
+                    codePoint and -0x200000 == 0 -> { // 4-byte sequence
+                        out.append((codePoint shr 18 and 0x07 or 0xF0).toByte())
+                        out.append((createByte(codePoint, 12)).toByte())
+                        out.append((createByte(codePoint, 6)).toByte())
+                    }
+                }
+                out.append((codePoint and 0x3F or 0x80).toByte())
+            }
+        }
 	}
 
 	override fun decode(out: StringBuilder, src: ByteArray, start: Int, end: Int) {
@@ -51,21 +83,32 @@ open class UTC8CharsetBase(name: String) : Charset(name) {
 			when (c shr 4) {
 				in 0..7 -> {
 					// 0xxxxxxx
-					out.append(c.toChar())
+					out.appendCodePoint(c)
 					i += 1
 				}
 				in 12..13 -> {
 					// 110x xxxx   10xx xxxx
-					out.append((c and 0x1F shl 6 or (src[i + 1].toInt() and 0x3F)).toChar())
+					out.appendCodePoint((c and 0x1F shl 6 or (src[i + 1].toInt() and 0x3F)))
 					i += 2
 				}
 				14 -> {
 					// 1110 xxxx  10xx xxxx  10xx xxxx
-					out.append((c and 0x0F shl 12 or (src[i + 1].toInt() and 0x3F shl 6) or (src[i + 2].toInt() and 0x3F)).toChar())
+					out.appendCodePoint((c and 0x0F shl 12 or (src[i + 1].toInt() and 0x3F shl 6) or (src[i + 2].toInt() and 0x3F)))
 					i += 3
 				}
+                15 -> {
+                    // 1111 0xxx 10xx xxxx  10xx xxxx  10xx xxxx
+                    out.appendCodePoint(0
+                        .insert(src[i + 0].toInt().extract(0, 3), 18, 3)
+                        .insert(src[i + 1].toInt().extract(0, 6), 12, 6)
+                        .insert(src[i + 2].toInt().extract(0, 6), 6, 6)
+                        .insert(src[i + 3].toInt().extract(0, 6), 0, 6)
+                    )
+                    i += 4
+
+                }
 				else -> {
-					i += 1
+                    TODO("${c shr 4}")
 				}
 			}
 		}
