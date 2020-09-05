441bfdd42e8b7456368cec3c3a49372aa3970273
soywiz <soywiz@gmail.com>
2020-09-05 08:52:32 +0200

    Try to circumvent issues with Kotlin 1.4

--- korio/src/nativeCommonMain/kotlin/com/soywiz/korio/net/asyncSocketFactoryNative.kt
+++ korio/src/nativeCommonMain/kotlin/com/soywiz/korio/net/asyncSocketFactoryNative.kt
@@ -1,3 +1,3 @@
 package com.soywiz.korio.net
 
-internal actual val asyncSocketFactory: AsyncSocketFactory = NativeAsyncSocketFactory
+//internal actual val asyncSocketFactory: AsyncSocketFactory = NativeAsyncSocketFactory
