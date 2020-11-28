088d75e203abe00632e1654e3f7ed061e43c239f
soywiz <soywiz@gmail.com>
2020-11-28 17:09:30 +0100

    Try to fix tests

--- korio/src/commonMain/kotlin/com/soywiz/korio/file/std/DynamicRootVfs.kt
+++ korio/src/commonMain/kotlin/com/soywiz/korio/file/std/DynamicRootVfs.kt
@@ -4,7 +4,7 @@ import com.soywiz.korio.file.*
 
 fun DynamicRootVfs(base: Vfs, rootGet: () -> String) = DynamicRootVfsVfs(base, rootGet).root
 
-class DynamicRootVfsVfs(val base: Vfs, val rootGet: () -> String) : Vfs.Proxy() {
+open class DynamicRootVfsVfs(val base: Vfs, val rootGet: () -> String) : Vfs.Proxy() {
     private fun getLocalAbsolutePath(path: String) = rootGet().pathInfo.lightCombine(path.pathInfo).fullPath
     override fun getAbsolutePath(path: String): String = base.getAbsolutePath(getLocalAbsolutePath(path))
     override suspend fun access(path: String): VfsFile = base[getLocalAbsolutePath(path)]
